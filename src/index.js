8a36e8401c52085ff52b8873f14161a8251b11e3
Mehdi <mehdi@example.com>
2026-01-07 17:23:19 +0100

    feat: initial project setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import { isProduction as checkIsProdution } from "./utils"; //-> import nommer et utilisation de "as" pour renommer renommer
+
+// import database from "db.js";
+
+// import dynamique
+// const test = async () => {
+// let test = await import("./db.js");
+// console.log(test);
+// };
