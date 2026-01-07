e8b01520e03cc314a5f23a9ca8429d1f5f7bceb8
Mehdi <mehdi@example.com>
2026-01-07 17:32:12 +0100

    feat: initial project setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
-  app.innerHTML = `<h1 class="title"> Hello world</>`;
+  app.innerHTML = `<h1 class="title"> Hello world</h1>`;
 });
 
 window.addEventListener("error", (event) => {
